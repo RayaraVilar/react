40271d1d733530408c8bccac49b746635c707fb9
Rayara <ravtra@hotmail.com>
2023-07-31 18:02:43 -0300

    mudanças do formulário

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css"
+
+function Banner(){
+    //JSX = parece html mas não é
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner principal da página organo"/>
+        </header>
+    )
+}
+
+export default Banner
